e408b9f325b7c5051c83ed1dbee1c4e74d5c1392
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-10-22 19:00:22 +0700

    Add files via upload

---
+++ IF-1-10118007-Latihan48-Kalkulator/src/kalkulator/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulator;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rama Al Halik
+ * KELAS : IF 1
+ * NIM   : 10118007
+ * Deskripsi Program : Program ini untuk membuat operasi kalkulator.
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Kalkulator objKal = new Kalkulator();
+        objKal.setValue1(7.0);
+        objKal.setValue2(5.0);
+        System.out.println("VALUE 1 = "+objKal.getValue1());
+        System.out.println("VALUE 2 = "+objKal.getValue2());
+        objKal.setNameProject();
+        objKal.setNoteProject("");
+        System.out.println("Result Add is = "+objKal.add());
+        System.out.println("Result Minus is = "+objKal.minus());
+        System.out.println("Result Multiple is = "+objKal.multiplication());
+        System.out.println("Result Division is = "+objKal.division());
+    }
+    
+}
